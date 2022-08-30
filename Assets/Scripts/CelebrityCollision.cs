6a00051267cf79158a161ab9f04a47aec22b7932
anmolbajpai24 <anmolbajpai24@gmail.com>
2022-08-30 23:49:02 +0530

    Added collision script (not functional yet)

---
+++ Assets/Scripts/CelebrityCollision.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CelebrityCollision : MonoBehaviour
+{
+    //public UnityEvent collision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    public void DestroyCeleb()
+    {
+        
+    }
+}
